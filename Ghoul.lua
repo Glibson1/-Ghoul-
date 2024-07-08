7c7b07147d2923ad57cc87782d21e0b6fa22ea77
Glibson1 <glibsonmanage@gmail.com>
2024-07-08 18:28:00 -0400

    Update Ghoul.lua

--- Ghoul.lua
+++ Ghoul.lua
@@ -1,11 +1,11 @@
-if place and place.id == 17639412521 then
+if game.PlaceId  == 17639412521 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Glibson1/-Ghoul-/main/REXL.lua"))()
-elseif place and place.id == 1499872953 then
+elseif game.PlaceId  == 1499872953 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Glibson1/-Ghoul-/main/REXL.lua"))()
-elseif place and place.id == 6897167394 then
+elseif game.PlaceId == 6897167394 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Glibson1/-Ghoul-/main/REXL.lua"))()
-elseif place and place.id == 14115013144 then
+elseif game.PlaceId == 14115013144 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Glibson1/-Ghoul-/main/REXL.lua"))()
-elseif place and place.id == 15558033314 then
+elseif game.PlaceId == 15558033314 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Glibson1/-Ghoul-/main/REXL.lua"))()
 end
