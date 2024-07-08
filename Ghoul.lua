87afe5124bfd2f860f49416bbed1b98d991948e3
Glibson1 <glibsonmanage@gmail.com>
2024-07-08 19:16:09 -0400

    Create Ghoul.lua

---
+++ Ghoul.lua
@@ -0,0 +1,81 @@
+if game.PlaceId == 17639412521 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Glibson1/-Ghoul-/main/REXL.lua"))()
+    Rayfield:Notify({
+        Title = "Loaded /Ghoul™",
+        Content = "/Ghoul™ has been loaded for this place!",
+        Duration = 6.5,
+        Image = 4483362458,
+        Actions = { -- Notification Buttons
+            Ignore = {
+                Name = "Okay!",
+                Callback = function()
+                    print("The user tapped Okay!")
+                end
+            },
+        },
+    })
+elseif game.PlaceId == 1499872953 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Glibson1/-Ghoul-/main/REXL.lua"))()
+    Rayfield:Notify({
+        Title = "Loaded /Ghoul™",
+        Content = "/Ghoul™ has been loaded for this place!",
+        Duration = 6.5,
+        Image = 4483362458,
+        Actions = { -- Notification Buttons
+            Ignore = {
+                Name = "Okay!",
+                Callback = function()
+                    print("The user tapped Okay!")
+                end
+            },
+        },
+    })
+elseif game.PlaceId == 6897167394 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Glibson1/-Ghoul-/main/REXL.lua"))()
+    Rayfield:Notify({
+        Title = "Loaded /Ghoul™",
+        Content = "/Ghoul™ has been loaded for this place!",
+        Duration = 6.5,
+        Image = 4483362458,
+        Actions = { -- Notification Buttons
+            Ignore = {
+                Name = "Okay!",
+                Callback = function()
+                    print("The user tapped Okay!")
+                end
+            },
+        },
+    })
+elseif game.PlaceId == 14115013144 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Glibson1/-Ghoul-/main/REXL.lua"))()
+    Rayfield:Notify({
+        Title = "Loaded /Ghoul™",
+        Content = "/Ghoul™ has been loaded for this place!",
+        Duration = 6.5,
+        Image = 4483362458,
+        Actions = { -- Notification Buttons
+            Ignore = {
+                Name = "Okay!",
+                Callback = function()
+                    print("The user tapped Okay!")
+                end
+            },
+        },
+    })
+elseif game.PlaceId == 15558033314 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Glibson1/-Ghoul-/main/REXL.lua"))()
+    Rayfield:Notify({
+        Title = "Loaded /Ghoul™",
+        Content = "/Ghoul™ has been loaded for this place!",
+        Duration = 6.5,
+        Image = 4483362458,
+        Actions = { -- Notification Buttons
+            Ignore = {
+                Name = "Okay!",
+                Callback = function()
+                    print("The user tapped Okay!")
+                end
+            },
+        },
+    })
+end
