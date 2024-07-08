d46a0378c605e8f050508058133eb2e961702130
Glibson1 <glibsonmanage@gmail.com>
2024-07-08 18:26:21 -0400

    Update Ghoul.lua

--- Ghoul.lua
+++ Ghoul.lua
@@ -1,11 +1,11 @@
-if place.id == 17639412521 then
-	loadstring(game:HttpGet("https://github.com/Glibson1/-Ghoul-/raw/main/REXL.lua"))()
-elseif place.id == 1499872953 then
-	loadstring(game:HttpGet("https://github.com/Glibson1/-Ghoul-/raw/main/REXL.lua"))()
-elseif place.id == 6897167394 then
-	loadstring(game:HttpGet("https://github.com/Glibson1/-Ghoul-/raw/main/REXL.lua"))()
-elseif place.id == 14115013144 then
-	loadstring(game:HttpGet("https://github.com/Glibson1/-Ghoul-/raw/main/REXL.lua"))()
-elseif place.id == 15558033314 then
-	loadstring(game:HttpGet("https://github.com/Glibson1/-Ghoul-/raw/main/REXL.lua"))()
+if place and place.id == 17639412521 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Glibson1/-Ghoul-/main/REXL.lua"))()
+elseif place and place.id == 1499872953 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Glibson1/-Ghoul-/main/REXL.lua"))()
+elseif place and place.id == 6897167394 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Glibson1/-Ghoul-/main/REXL.lua"))()
+elseif place and place.id == 14115013144 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Glibson1/-Ghoul-/main/REXL.lua"))()
+elseif place and place.id == 15558033314 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Glibson1/-Ghoul-/main/REXL.lua"))()
 end
