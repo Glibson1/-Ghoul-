cf2c79fb34664dbb2f0ff1d60a7265e554876676
Glibson1 <glibsonmanage@gmail.com>
2024-07-08 18:21:35 -0400

    Update Ghoul™.lua

--- Ghoul™.lua
+++ Ghoul™.lua
@@ -1,3 +1,11 @@
 if place.id == 17639412521 then
-	da
+	loadstring(game:HttpGet("https://github.com/Glibson1/-Ghoul-/raw/main/REXL.lua"))()
+elseif place.id == 1499872953 then
+	loadstring(game:HttpGet("https://github.com/Glibson1/-Ghoul-/raw/main/REXL.lua"))()
+elseif place.id == 6897167394 then
+	loadstring(game:HttpGet("https://github.com/Glibson1/-Ghoul-/raw/main/REXL.lua"))()
+elseif place.id == 14115013144 then
+	loadstring(game:HttpGet("https://github.com/Glibson1/-Ghoul-/raw/main/REXL.lua"))()
+elseif place.id == 15558033314 then
+	loadstring(game:HttpGet("https://github.com/Glibson1/-Ghoul-/raw/main/REXL.lua"))()
 end
